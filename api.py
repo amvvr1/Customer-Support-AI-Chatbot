de29c4fd0e17ae37ced4e2e746e905bf67183082
ammar <scholarammar@gmail.com>
2025-11-01 19:23:19 +0100

    hello

---
+++ api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from main import agent
+from pydantic import BaseModel
+
+class Request(BaseModel):
+    message : str 
+    thread_id: str = "1"
+
+app = FastAPI()
+    
+app.add_middleware(CORSMiddleware, allow_origins= ["*"])
+
+@app.post("/chat")
+def chat(request: Request):
+    response = agent.invoke(
+        {"messages": [{"role": "user", "content": request.message}]},
+        {"configurable": {"thread_id": request.thread_id}}
+    )
+    
+    return {"response": response["messages"][-1].content}

847404e1290c215faba072f89102294c172f4161
ammar <scholarammar@gmail.com>
2025-11-02 02:13:19 +0100

    lololo

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.document_loaders import JSONLoader, CSVLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langgraph.checkpoint.memory import InMemorySaver
 from langchain.tools import tool
 from langchain.agents import create_agent
@@ -15,49 +14,39 @@ llm = init_chat_model(model="gpt-4o-mini")
 
 embed_model = OpenAIEmbeddings(model="text-embedding-3-small")
 
-docs = []
-
 json_doc = JSONLoader(file_path="docs/faq.json",
                        jq_schema=".faqs[]", 
                        text_content=False)
-
 faq_docs = json_doc.load()
 
 csv_doc = CSVLoader(file_path="docs/products.csv")
-
 product_doc = csv_doc.load()
 
 
-docs.append(faq_docs)
-docs.append(product_doc)
-
-docs = [doc for sublist in docs for doc in sublist]
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000,
-    chunk_overlap = 200, 
-    add_start_index = True
-)
-
-all_splits = text_splitter.split_documents(docs)
+faq_store = Chroma(collection_name="faq", embedding_function=embed_model)
+product_store = Chroma(collection_name="products", embedding_function=embed_model)
 
 
-vector_store = Chroma(collection_name="chatbot", 
-                      embedding_function=embed_model, 
-                      persist_directory="./chroma_langchain_db",)
-
-doc_ids = vector_store.add_documents(documents=all_splits)
+faq_store.add_documents(faq_docs)
+product_store.add_documents(product_doc)
 
 
-@tool(response_format="content_and_artifact")
-def retrieve_context(query: str): 
-    """retrieve info to help answer a query"""
-    retrieved_docs = vector_store.similarity_search(query=query, k=2)
+@tool
+def answer_faq(query: str): 
+    """retrieve info to help answer a FAQ related data"""
+    retrieved_docs = faq_store.similarity_search(query=query, k=2)
     serialized = "\n\n".join(
         (f"Source: {doc.metadata}\nContent: {doc.page_content}")
         for doc in retrieved_docs
     )
     return serialized, retrieved_docs
+
+@tool
+def give_product_info(query: str):
+    """search for product information and give product recommendations"""
+    retrieved_docs = product_store.similarity_search(query=query, k=5)
+    return "\n\n".join(f"{doc.page_content}" for doc in retrieved_docs)
+
 
 order_df = pd.read_csv("docs/order_data_50.csv")
 
@@ -76,7 +65,7 @@ def check_order_status(order_id: str):
         f"Available Sizes: {row['available_sizes']}"
     )
 
-tools = [retrieve_context, check_order_status]
+tools = [answer_faq, give_product_info, check_order_status]
 
 with open("system_prompt.txt", "r") as f:
     prompt = f.read()
